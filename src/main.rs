15ec8ad321492cf94b594c7c4f27439496078e5b
Matthew Li <matthew.li10@education.nsw.gov.au>
2023-07-18 14:23:11 +1000

    add rocket and main file

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use rocket::fs::FileServer;
+
+#[macro_use]
+extern crate rocket;
+
+#[catch(404)]
+fn not_found() -> &'static str {
+    "We couldn't find that page.
+(404 - Not Found)"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/api/v1/", FileServer::from("static"))
+        .register("/", catchers![not_found])
+}
