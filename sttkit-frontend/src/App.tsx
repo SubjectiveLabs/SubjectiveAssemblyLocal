c54b1d3e76d23b635e67e3b907c401a51f5d5314
Matthew Li <matthew.li10@education.nsw.gov.au>
2023-07-18 16:58:58 +1000

    add a workspace and a vite react template (circles-png/vite-project)

---
+++ sttkit-frontend/src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => <>
+  <h1 className='grid place-content-center h-full'>hello world</h1>
+</>
+
+export default App
---
+++ sttkit-frontend/src/main.tsx
@@ -0,0 +1,10 @@
+import './index.css'
+import App from 'App'
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
