5a2c3fc1ae9c158a01f7fb580e0f1ff32e5dae3a
Matthew Li <matthew.li10@education.nsw.gov.au>
2023-07-19 12:20:40 +1000

    update app and add a header component

--- sttkit-frontend/src/App.tsx
+++ sttkit-frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import Divider from 'Divider'
+import Header from 'Header'
 
 const App = () => {
   const dayEndings = [
@@ -20,18 +21,23 @@ const App = () => {
           'Su'
         ]
   return <>
-    <div className='h-full bg-cinder-950 flex flex-col p-6 gap-6 font-extrabold tracking-tighter'>
-      <header className='text-center flex items-baseline gap-3 justify-center'>
-        <h1 className='text-gold-500 tracking-[-5px] text-4xl leading-none'>SchoolScout</h1>
-        <h2 className='text-gold-200 text-2xl leading-none'>Assembly</h2>
-      </header>
+    <div className='h-full bg-cinder-950 flex flex-col p-4 gap-6 font-extrabold tracking-tighter'>
+      <Header />
       <Divider />
-      <div className='flex items-center'>
-        <span className='text-gold-200 rotate-90 inline-block text-2xl leading-none -translate-x-5 py-6'>
+      <div className='flex items-center my-6'>
+        <span className='text-gold-200 rotate-90 absolute text-2xl leading-none -translate-x-5'>
           DAYS
         </span>
-        <div className='text-gold-100 flex justify-between grow'>
-          {daysShort.map(day => <span key={day} className='border-[3px] rounded-full w-10 h-10 grid place-content-center border-gold-200'>{day}</span>)}
+        <div className='text-gold-100 flex justify-between grow ps-12'>
+          {daysShort.map(day => <span
+            key={day}
+            className='border-[3px] rounded-full w-10 h-10 grid place-content-center border-gold-200 hover:bg-gold-200 hover:text-indigo-600 transition duration-300 hover:scale-[1.25]'
+            onClick={() => {
+              document.querySelectorAll('*').forEach(element => {
+                element.className += ' rotate-[1000deg] transition duration-[10s]'
+              })
+            }}
+          >{day}</span>)}
         </div>
       </div>
       <Divider />
