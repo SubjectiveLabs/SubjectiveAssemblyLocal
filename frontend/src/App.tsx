97f34a7a72a8f8fafbde9e6c31bde4431cca8df9
Matthew Li <matthew.li10@education.nsw.gov.au>
2023-08-10 21:58:18 +1000

    update app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import Footer from 'Footer'
 import config from 'config.json'
+import { useState } from 'react'
 
 type Notice = {
   content: string,
@@ -28,9 +29,10 @@ const App = () => {
       time    : 'Until tomorrow',
       title   : 'Y9 2024 Subject Selection'
     }
-  ]
+  ],
+        [ active, setActive ] = useState(0)
   return <>
-    <div className='p-4 bg-gray-50 h-full flex flex-col gap-4 font-semibold tracking-tighter leading-none'>
+    <div className='py-4 bg-gray-50 h-full flex flex-col gap-4 font-semibold tracking-tighter leading-none'>
       <header className='text-center flex flex-col shrink grow-0 basis-auto'>
         <h1>Welcome to</h1>
         <h1 className='flex items-center text-xl gap-2 justify-center font-bold'>
@@ -38,37 +40,46 @@ const App = () => {
           {config.schoolName}
         </h1>
       </header>
-      <div className='border shadow-lg rounded-2xl grow shrink-0 basis-auto p-4 flex flex-col gap-2'>
-        <div className='gap-2 items-center flex text-xl'>
-          <span className='bg-black w-6 h-6 inline-flex rounded-full rounded-bl-none'></span>
-          Messaging
+      <div className='flex h-full overflow-x-auto snap-mandatory snap-x scroll-smooth gap-8 p-4' onScroll={event => {
+        const scroll = event.currentTarget.scrollLeft / event.currentTarget.scrollWidth * 2
+        if (scroll === Math.floor(scroll))
+          setActive(Math.floor(scroll))
+      }}>
+        <div className='border shadow-lg rounded-2xl grow shrink-0 basis-auto p-4 flex flex-col gap-2 w-full snap-center'>
+          <div className='gap-2 items-center flex text-xl'>
+            <span className='bg-black w-6 h-6 inline-flex rounded-full rounded-bl-none'></span>
+            Messaging
+          </div>
+          <div className='flex gap-1'>
+            <span className='text-gray-500'>Noticeboard</span>
+            <span>{notices.length} notices</span>
+          </div>
+          <ul className='flex flex-col gap-4 overflow-y-auto shadow-inner rounded-2xl'>
+            {
+              notices.map((notice, index) => <div className='border shadow-lg rounded-2xl p-4' key={index}>
+                <h2 className='font-bold flex gap-1 items-center'>
+                  {
+                    notice.priority && <span className='text-red-400 flex items-center gap-1'>
+                      <span className='font-extrabold text-xl'>!</span>
+                    Priority
+                    </span>
+                  }
+                  {notice.title}
+                </h2>
+                <p className='text-gray-600 text-xm'>{notice.content}</p>
+                <span className='text-gray-400 text-sm flex gap-1 items-center'>
+                  <span className='bg-gray-400 w-3 h-3 inline-flex rounded-full'></span>
+                  {notice.time}
+                </span>
+              </div>)
+            }
+          </ul>
         </div>
-        <div className='flex gap-1'>
-          <span className='text-gray-500'>Noticeboard</span>
-          <span>{notices.length} notices</span>
+        <div className='border shadow-lg rounded-2xl grow shrink-0 basis-auto p-4 flex flex-col gap-2 w-full snap-center'>
+          content
         </div>
-        <ul className='flex flex-col gap-4'>
-          {
-            notices.map((notice, index) => <div className='border shadow-lg rounded-2xl p-4' key={index}>
-              <h2 className='font-bold flex gap-1 items-center'>
-                {
-                  notice.priority && <span className='text-red-400 flex items-center gap-1'>
-                    <span className='font-extrabold text-xl'>!</span>
-                  Priority
-                  </span>
-                }
-                {notice.title}
-              </h2>
-              <p className='text-gray-600 text-xm'>{notice.content}</p>
-              <span className='text-gray-400 text-sm flex gap-1 items-center'>
-                <span className='bg-gray-400 w-3 h-3 inline-flex rounded-full'></span>
-                {notice.time}
-              </span>
-            </div>)
-          }
-        </ul>
       </div>
-      <Footer active={0}/>
+      <Footer active={active}/>
     </div>
   </>
 }
--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 import classNames from 'classNames'
 
-const Footer = ({ active }: {active:number}) => <footer className='flex justify-center shrink grow-0 basis-auto items-center gap-1 pb-4'>
+const Footer = ({ active }: {active:number}) => <footer className='flex justify-center shrink grow-0 basis-auto items-center gap-1 border-t p-4'>
   {
     [...Array(2)].map((_value, index) => <span
       className={classNames(
