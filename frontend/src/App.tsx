7e9e8035a946e3c28b5631bf86ab119c6f4c0009
Matthew Li <matthew.li10@education.nsw.gov.au>
2023-08-10 22:21:20 +1000

    basic bell display

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,27 +9,95 @@ type Notice = {
   title: string,
 }
 
+type Time = {
+  hour: number
+  minute: number
+}
+
+type Bell = {
+  name: string,
+  time: Time,
+}
+
 const App = () => {
-  const notices: Notice[] = [
+  const bells: Bell[] = [
     {
-      content : 'D13 & D14 classes will be running in B2 as carpets are replaced.',
-      priority: true,
-      time    : 'Today',
-      title   : 'D Block Room Changes'
+      name: 'Period 1',
+      time: {
+        hour  : 8,
+        minute: 38
+      }
     },
     {
-      content : 'French will be off today due to a Year 11 excursion occuring. See you next week!',
-      priority: false,
-      time    : 'Until 12th September',
-      title   : 'French Club'
+      name: 'Period 2',
+      time: {
+        hour  : 9,
+        minute: 21
+      }
     },
     {
-      content : 'Meeting tomorrow lunch in TLC for Year 9 will be running outlining the subject selection process for 2024.',
-      priority: false,
-      time    : 'Until tomorrow',
-      title   : 'Y9 2024 Subject Selection'
+      name: 'Recess',
+      time: {
+        hour  : 11,
+        minute: 20
+      }
+    },
+    {
+      name: 'Period 3',
+      time: {
+        hour  : 12,
+        minute: 34
+      }
+    },
+    {
+      name: 'Period 4',
+      time: {
+        hour  : 13,
+        minute: 5
+      }
+    },
+    {
+      name: 'Period 5',
+      time: {
+        hour  : 14,
+        minute: 34
+      }
+    },
+    {
+      name: 'Lunch',
+      time: {
+        hour  : 15,
+        minute: 0
+      }
+    },
+    {
+      name: 'Period 6',
+      time: {
+        hour  : 15,
+        minute: 38
+      }
     }
   ],
+        notices: Notice[] = [
+          {
+            content : 'D13 & D14 classes will be running in B2 as carpets are replaced.',
+            priority: true,
+            time    : 'Today',
+            title   : 'D Block Room Changes'
+          },
+          {
+            content : 'French will be off today due to a Year 11 excursion occuring. See you next week!',
+            priority: false,
+            time    : 'Until 12th September',
+            title   : 'French Club'
+          },
+          {
+            content : 'Meeting tomorrow lunch in TLC for Year 9 will be running outlining the subject selection process for 2024.',
+            priority: false,
+            time    : 'Until tomorrow',
+            title   : 'Y9 2024 Subject Selection'
+          }
+        ],
         [ active, setActive ] = useState(0)
   return <>
     <div className='py-4 bg-gray-50 h-full flex flex-col gap-4 font-semibold tracking-tighter leading-none'>
@@ -45,7 +113,7 @@ const App = () => {
         if (Math.abs(scroll - Math.round(scroll)) < 10)
           setActive(Math.round(scroll))
       }}>
-        <div className='border shadow-lg rounded-2xl grow shrink-0 basis-auto p-4 flex flex-col gap-2 w-full snap-center'>
+        <div className='border shadow-lg rounded-2xl grow shrink-0 basis-auto p-4 flex flex-col gap-2 w-full snap-center bg-white'>
           <div className='gap-2 items-center flex text-xl'>
             <span className='bg-black w-6 h-6 inline-flex rounded-full rounded-bl-none'></span>
             Messaging
@@ -56,7 +124,7 @@ const App = () => {
           </div>
           <ul className='flex flex-col gap-4 overflow-y-auto shadow-inner rounded-2xl'>
             {
-              notices.map((notice, index) => <div className='border shadow-lg rounded-2xl p-4' key={index}>
+              notices.map((notice, index) => <div className='border rounded-2xl p-4' key={index}>
                 <h2 className='font-bold flex gap-1 items-center'>
                   {
                     notice.priority && <span className='text-red-400 flex items-center gap-1'>
@@ -75,8 +143,26 @@ const App = () => {
             }
           </ul>
         </div>
-        <div className='border shadow-lg rounded-2xl grow shrink-0 basis-auto p-4 flex flex-col gap-2 w-full snap-center'>
-          content
+        <div className='border shadow-lg rounded-2xl grow shrink-0 basis-auto p-4 flex flex-col gap-2 w-full snap-center bg-white'>
+          <div className='gap-2 items-center flex text-xl'>
+            <span className='bg-black w-4 h-4 inline-flex rotate-45'></span>
+            Bell Times
+          </div>
+          <div className='flex gap-1 justify-between items-center'>
+            <span className='text-gray-500'>Today</span>
+            <span className='bg-blue-500 p-2 rounded-full text-white flex items-center gap-2'>
+              <span className='bg-white w-4 h-4 inline-flex rounded-full'></span>
+              Showing Monday bell times.
+            </span>
+          </div>
+          <ul className='flex flex-col gap-4 overflow-y-auto shadow-inner rounded-2xl'>
+            {
+              bells.map((bell, index) => <div className='border rounded-2xl p-4 flex justify-between' key={index}>
+                <span>{bell.name}</span>
+                <span>{bell.time.hour}:{bell.time.minute}</span>
+              </div>)
+            }
+          </ul>
         </div>
       </div>
       <Footer active={active}/>
