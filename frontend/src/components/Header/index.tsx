2c07928cef4abc49f80956430a34636a31b45623
Matthew Li <matthew.li10@education.nsw.gov.au>
2023-09-08 21:38:35 +1000

    update school name input

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -5,14 +5,15 @@ import classNames from "utils/classNames";
 const Header = () => {
   const [school, setSchool, , setShowPassword] = useContext(AppContext)
   const [focused, setFocused] = useState(false);
+  const [name, setName] = useState(school.name);
   return <header className='text-center flex flex-col shrink grow-0 basis-auto items-center w-full px-4 gap-4 md:flex-row'>
-    <div className="w-64">
+    <div className="w-32">
 
     </div>
-    <div className="flex flex-col basis-auto w-full gap-2">
+    <div className="flex flex-col basis-auto w-full gap-2 relative">
       <h1>Welcome to</h1>
       <span className={classNames(
-        "rounded-xl p-2 w-full text-xl font-bold flex gap-4 items-center pl-[43px] transition",
+        "rounded-xl p-2 w-full text-xl font-bold flex gap-4 items-center transition",
         focused ? 'bg-gray-200' : ''
       )}>
         <input
@@ -30,11 +31,19 @@ const Header = () => {
               return next
             })
           }}
+          onChange={event => {
+            setName(event.target.value)
+          }}
           defaultValue={school.name}
         />
-        <svg viewBox="0 0 16 16" width={32} height={32}>
-          <path
-            d="
+        <div className="absolute text-center inset-x-0 flex justify-center items-center pointer-events-none gap-2 tracking-normal">
+          <div className="w-8 h-8"></div>
+          <span className="invisible">{name || 'School Name'}</span>
+          <svg viewBox="0 0 16 16" width={32} height={32} className={classNames(
+            focused ? 'invisible' : ''
+          )}>
+            <path
+              d="
               M 1 15
               l 3 -1
               l 8 -8
@@ -49,12 +58,13 @@ const Header = () => {
               a 1 1 0 0 0 -1 0
               z
             "
-            className="fill-neutral-500"
-          />
-        </svg>
+              className="fill-neutral-500"
+            />
+          </svg>
+        </div>
       </span>
     </div>
-    <div className="flex gap-4 w-64 justify-center md:justify-end">
+    <div className="flex gap-4 w-32 justify-center md:justify-end">
       <svg
         viewBox="0 0 16 16"
         width={32}
