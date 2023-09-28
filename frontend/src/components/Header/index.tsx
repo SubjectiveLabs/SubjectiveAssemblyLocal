2a55405bec04e5047cd6e911534da5bf4a710b51
Matthew Li <matthew.li10@education.nsw.gov.au>
2023-09-28 13:33:48 +1000

    fix pencil

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import { AppContext } from "App"
 import { Door, Heart, Key, Pencil } from "Icons";
-import { useContext, useEffect, useState } from "react"
+import { useContext, useEffect, useRef, useState } from "react"
 import classNames from "classNames";
 
 const Header = () => {
@@ -10,11 +10,21 @@ const Header = () => {
     thanks,
     setShowSettings
   } = useContext(AppContext)
-  const [focused, setFocused] = useState(false);
-  const [name, setName] = useState(school.name);
+  const [focused, setFocused] = useState(false)
+  const [name, setName] = useState(school.name)
+  const input = useRef<HTMLInputElement>(null)
+  const fakeText = useRef<HTMLSpanElement>(null)
+  const [inputWidth, setInputWidth] = useState(fakeText.current?.clientWidth || 0)
+  const [fakeTextWidth, setFakeTextWidth] = useState(fakeText.current?.clientWidth || 0)
   useEffect(() => {
     setName(school.name)
   }, [school])
+  useEffect(() => {
+    addEventListener('resize', () => {
+      setInputWidth(input.current?.clientWidth || 0)
+      setFakeTextWidth(fakeText.current?.clientWidth || 0)
+    })
+  }, [])
   return <header className='text-center flex flex-col shrink grow-0 basis-auto items-center w-full px-4 gap-x-4 md:flex-row'>
     <div className="w-72 hidden md:flex gap-2">
       <svg width={24} height={24} viewBox="0 0 16 16">
@@ -31,44 +41,58 @@ const Header = () => {
     </div>
     <div className="flex flex-col basis-auto w-full relative">
       <h1 className="md:text-xl">Welcome to</h1>
-      <span className={classNames(
-        "rounded-xl pl-1 w-full text-xl font-bold flex gap-x-4 items-center transition",
-        focused ? 'bg-gray-200' : ''
-      )}>
-        <input
-          type='text'
-          placeholder="School name"
-          className="bg-transparent w-full text-center text-xl md:text-3xl"
-          onFocus={() => {
-            setFocused(true)
-          }}
-          onBlur={event => {
-            setFocused(false)
-            setSchool(previous => {
-              const next = { ...previous }
-              next.name = event.target.value
-              return next
-            })
-          }}
-          onChange={event => {
-            setName(event.target.value)
-          }}
-          defaultValue={school.name}
-        />
-        <div className="absolute text-center inset-x-0 flex justify-center items-center pointer-events-none gap-2 tracking-normal">
-          <div className="w-8 h-8"></div>
-          <span className="invisible text-xl md:text-3xl">{name || 'School Name'}</span>
-          <svg viewBox="0 0 16 16" width={24} height={24} className={classNames(
-            'shrink-0 bg-gray-50',
-            focused ? 'invisible' : ''
-          )}>
-            <path
-              d={Pencil}
-              className="fill-neutral-500"
-            />
-          </svg>
-        </div>
-      </span>
+      <div className="flex items-center gap-4">
+        <span className={classNames(
+          "rounded-xl w-full text-xl font-bold flex gap-4 items-center",
+          focused ? 'bg-gray-200' : ''
+        )}>
+          <input
+            type='text'
+            placeholder="School name"
+            className="bg-transparent w-full text-center text-xl md:text-3xl overflow-auto pl-2"
+            onFocus={() => {
+              setFocused(true)
+            }}
+            onBlur={event => {
+              setFocused(false)
+              setSchool(previous => {
+                const next = { ...previous }
+                next.name = event.target.value
+                return next
+              })
+            }}
+            onChange={event => {
+              setName(event.target.value)
+            }}
+            defaultValue={school.name}
+            ref={input}
+          />
+          <div className="absolute text-center inset-x-0 flex justify-center items-center pointer-events-none gap-2 tracking-normal">
+            <div className="w-8 h-8"></div>
+            <span className="invisible text-xl md:text-3xl" ref={fakeText}>{name || 'School Name'}</span>
+            <svg viewBox="0 0 16 16" width={24} height={24} className={classNames(
+              'shrink-0 bg-gray-50',
+              focused ? 'invisible' : '',
+              inputWidth > fakeTextWidth ? 'visible' : 'invisible'
+            )}>
+              <path
+                d={Pencil}
+                className="fill-neutral-500"
+              />
+            </svg>
+          </div>
+        </span>
+        <svg viewBox="0 0 16 16" width={24} height={24} className={classNames(
+          'shrink-0 bg-gray-50',
+          focused ? 'invisible' : '',
+          inputWidth < fakeTextWidth ? 'visible' : 'invisible'
+        )}>
+          <path
+            d={Pencil}
+            className="fill-neutral-500"
+          />
+        </svg>
+      </div>
     </div>
     <div className="hidden gap-4 w-72 justify-end md:flex">
       <svg
